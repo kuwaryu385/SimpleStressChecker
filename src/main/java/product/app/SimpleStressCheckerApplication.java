ee1e30e01a3ce7d101755791808e47bd962f11c3
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-03-11 13:54:10 +0900

    Gradleプロジェクトの作成

---
+++ src/main/java/product/app/SimpleStressCheckerApplication.java
@@ -0,0 +1,13 @@
+package product.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStressCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStressCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/product/app/SimpleStressCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package product.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStressCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
