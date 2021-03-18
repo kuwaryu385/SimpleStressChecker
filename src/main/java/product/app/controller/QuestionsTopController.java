0cad5262d06260139daa45047a79654b469ab180
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-03-18 16:05:10 +0900

    仮の初期画面表示

---
+++ src/main/java/product/app/controller/QuestionsTopController.java
@@ -0,0 +1,52 @@
+package product.app.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import product.app.service.QuestionsService;
+
+@Controller
+@RequestMapping("/sqtop")
+public class QuestionsTopController {
+
+	@Autowired
+	private QuestionsService service;
+
+	@GetMapping
+	public String top(Model model) {
+
+		//問題数の取得
+		int questionCount = service.countQuestions();
+		model.addAttribute("questionsAllCount", questionCount);
+		return "top";
+	}
+
+	@PostMapping(value = "start")
+	public String start(RedirectAttributes redirectAttributes) {
+
+		//問題数の取得
+		int questionCount = service.countQuestions();
+
+		//出題テーブルを初期化
+		service.deleteQuestions();
+
+		//出題リスト
+		List<Integer> questionList = new ArrayList<>();
+		for (int i = 1; i <= questionCount; i++) {
+			questionList.add(i);
+		}
+		//出題リストに登録
+		service.insertQuestions(questionList);
+
+		return "redirect:/sqtop/question";
+	}
+
+}
