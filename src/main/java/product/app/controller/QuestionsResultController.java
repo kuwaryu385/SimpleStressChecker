47797331972b991a216e9f9682c7abae9c5042fb
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-03-24 16:14:45 +0900

    結果画面の実装、ストレス項目への回答結果処理

---
+++ src/main/java/product/app/controller/QuestionsResultController.java
@@ -0,0 +1,44 @@
+package product.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import product.app.service.QuestionsService;
+
+@Controller
+@RequestMapping("/sqtop/result")
+public class QuestionsResultController {
+
+	@Autowired
+	private QuestionsService service;
+
+	//結果処理
+	@GetMapping
+	public String result(Model model) {
+		//出題数を取得
+		int countmyquestion = service.countMyQuestions();
+		model.addAttribute("countmyquestion", countmyquestion);
+
+		//正解数を取得
+		int countreview = service.countReview();
+		model.addAttribute("countreview", countreview);
+
+		//正解率を算出
+		double answerrate = (double) countreview / countmyquestion * 100;
+		model.addAttribute("answerrate", answerrate);
+
+		if (answerrate >= 70) {
+			model.addAttribute("resultmsg", "あなたは高ストレス傾向です！！直ちに休息をとってください");
+		} else if (answerrate >= 30 && answerrate <= 60) {
+			model.addAttribute("resultmsg", "あなたは中ストレス傾向です！無理は禁物。意識的に休息を取りましょう！");
+		} else {
+			model.addAttribute("resultmsg", "あなたは低ストレス傾向です！日頃のケアに努めていきましょう");
+		}
+
+		return "result";
+	}
+
+}
