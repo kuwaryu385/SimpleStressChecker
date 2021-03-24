f744126192045d4ba7c0b380b7c473ca3534be40
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-03-24 16:09:00 +0900

    出題テーブルの追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,27 @@
 
+USE `stressquestion`;
+
 --同じテーブル名があっても複製できる
 CREATE TABLE IF NOT EXISTS questions(
           id          int(6)        NOT NULL PRIMARY KEY
          ,question    varchar (255) NOT NULL
-         ,choice_y    varchar (10)  NOT NULL
-         ,choice_n    varchar (10)  NOT NULL
+         ,choiceY    varchar (10)  NOT NULL
+         ,choiceN    varchar (10)  NOT NULL
          ,answer      int(3)        NOT NULL
+         ,answered     boolean      NOT NULL
          ,answerRate   boolean      NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS my_questions(
+		  id          int(6)        NOT NULL PRIMARY KEY
+         ,question    varchar (255) NOT NULL
+         ,choiceY    varchar (10)  NOT NULL
+         ,choiceN    varchar (10)  NOT NULL
+         ,answer      int(3)        NOT NULL
+         ,answered     boolean      NOT NULL
+         ,answerRate   boolean      NOT NULL
+);
+
+
 
 
