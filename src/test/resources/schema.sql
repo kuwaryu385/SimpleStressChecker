6402d44ce8f3eae10be87f4e0ce1939150227480
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-04-18 17:15:07 +0900

    データベースエラーにて設定変更

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS questions(
          ,answerRate   boolean      NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS my_questions(
-		  id          int(6)        NOT NULL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS my_questions
+(  id          int(6)        NOT NULL PRIMARY KEY
          ,question    varchar (255) NOT NULL
          ,choiceY    varchar (10)  NOT NULL
          ,choiceN    varchar (10)  NOT NULL
