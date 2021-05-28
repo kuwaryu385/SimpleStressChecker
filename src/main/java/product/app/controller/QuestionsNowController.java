1f81ac0f86069efaaa2cbba57a43e939369d44f0
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-05-28 11:19:41 +0900

    回答済みの問題を取得し、未解答の問題へ変更するIDの受け渡しする機能追加しました。

--- src/main/java/product/app/controller/QuestionsNowController.java
+++ src/main/java/product/app/controller/QuestionsNowController.java
@@ -28,6 +28,10 @@ public class QuestionsNowController {
 		List<Integer> questionIdlist = new ArrayList<Integer>();
 		questionIdlist = service.idQuestion();
 
+		//質問テーブルから回答済みのIDを取得
+		List<Integer> questionUesedIdlist = new ArrayList<Integer>();
+		questionUesedIdlist = service.idUesedQuestion();
+
 		//全問終了
 		if (questionIdlist.size() == 0) {
 			return "redirect:/result";
@@ -43,6 +47,13 @@ public class QuestionsNowController {
 
 		//出題
 		model.addAttribute("nowQuestion", service.selectOne(questionIdlist.get(0)));
+
+		//戻るボタンへ値を渡す。
+		if (questionUesedIdlist.size() == 0) {
+			model.addAttribute("uesedQuestion", null);
+		} else {
+			model.addAttribute("uesedQuestion", service.selectOne(questionUesedIdlist.get(0)));
+		}
 
 		return "/questions";
 
@@ -60,6 +71,15 @@ public class QuestionsNowController {
 		}
 		//回答済みにセット
 		service.answerdQuestion(id);
+
+		return "redirect:/questions";
+
+	}
+
+	@PostMapping(value = "oneBefor")
+	public String oneBefor(@RequestParam(name = "id", required = false) Integer id) {
+		//未回答の問題を取得
+		service.unanswerdQuestion(id);
 
 		return "redirect:/questions";
 
